7bd483089c6b0489ead7c663979835712e8f4915
mohtasim <333mohammedmohtasim@gmail.com>
2025-02-15 12:51:01 +0530

    setup frontend of login page

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,31 +1,139 @@
-import React from 'react'
-import { LoginBg, Logo } from '../assets';
-//for method 1 
+import React, { useState } from "react";
+import { LoginBg, Logo } from "../assets";
+import { LoginInput } from "../components";
+import { FaEnvelope, FaLock, FcGoogle } from "../assets/icons";
+import { motion } from "framer-motion";
+import { buttonClick } from "../animations";
+
+//for method 1
 // export const Login = () => {
 //   return (
 //     <div>login</div>
 //   )
 // }
+
 //for method 2
 const Login = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [isSignUp, setisSignUp] = useState(false);
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+
   return (
     <div className="w-screen h-screen relative overflow-hidden flex">
-      
-    {/*background image*/}
-    <img 
-    src={LoginBg} 
-    className="w-full h-full object-cover absolute " 
-    alt=""
-    />  
+      {/*background image*/}
+      <img
+        src={LoginBg}
+        className="w-full h-full object-cover absolute top-0 left-0 "
+        alt=""
+      />
+
       {/*content box*/}
-      <div
-      className="flex flex-col items-center bg-lightOverlay w-[25%] md:w-208 h-full z-10 backdrop-blur-md p-4 px-4 py-12" >
+      <div className="flex flex-col items-center bg-lightOverlay w-[30%]   h-full z-10 backdrop-blur-md p-4 px-4 py-12 gap-6">
         <div className="flex items-center justify-start gap-4 w-full">
-          <img src={Logo}  className='w-8' alt="" />
-          <p className='font-semibold text-2xl'>City</p>
+          <img src={Logo} className="w-8" alt="" />
+          <p className="text-headingColor font-semibold text-2xl">City</p>
         </div>
-      </div>  
+
+        {/*Welcome section*/}
+        <p className=" text-3xl text-red-500 font-bold">Welcome Back!</p>
+
+        <p className=" text-xl -mt-2 text-textColor">
+          {isSignUp ? "Sign Up " : "Sign in"}
+        </p>
+
+        {/* input section */}
+        <div className=" w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-4">
+          <LoginInput
+            placeholder="Email here"
+            icon={<FaEnvelope className="text-xl text-textColor" />}
+            inputState={userEmail}
+            inputStateFunc={setUserEmail}
+            type="email"
+            isSignUp={isSignUp}
+          />
+
+          <LoginInput
+            placeholder="Password"
+            icon={<FaLock className="text-xl text-textColor" />}
+            inputState={password}
+            inputStateFunc={setpassword}
+            type="password"
+            isSignUp={isSignUp}
+          />
+
+          {isSignUp && (
+            <LoginInput
+              placeholder="Confirm Password"
+              icon={<FaLock className="text-xl text-textColor" />}
+              inputState={confirmPassword}
+              inputStateFunc={setconfirmPassword}
+              type="password"
+              isSignUp={isSignUp}
+            />
+          )}
+
+          {!isSignUp ? (
+            <p className="font-semibold text-2xl">
+              Don't have an account :{/* friend operator ... */}
+              <motion.button
+                {...buttonClick}
+                className="text-green-500 underline cursor-pointer bg-transparent"
+                // // whileHover={{ scale: 1.5 }}
+                // whileTap={{ scale: 0.9 }}
+                onClick={() => setisSignUp(true)}
+              >
+                Sign Up here!
+              </motion.button>
+            </p>
+          ) : (
+            <p className="font-semibold text-2xl">
+              Already have an account, Great :) {/* friend operator ... */}
+              <motion.button
+                {...buttonClick}
+                className="text-green-500 underline cursor-pointer bg-transparent"
+                onClick={() => setisSignUp(false)}
+              >
+                Sign in here !
+              </motion.button>
+            </p>
+          )}
+
+          {/* Button section */}
+          {isSignUp ? (
+            <motion.button
+              {...buttonClick}
+              className=" w-full px-4 py-2 rounded-lg bg-red-400 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150"
+            >
+              Sign Up
+            </motion.button>
+          ) : (
+            <motion.button
+              {...buttonClick}
+              className=" w-full px-4 py-2 rounded-lg bg-red-400 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150"
+            >
+              Sign In
+            </motion.button>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between gap-16">
+          <div className="w-24 h-[1px] bg-white rounded-md"></div>
+          <p className="text-white text-2xl">OR</p>
+          <div className="w-24 h-[1px] bg-white rounded-md"></div>
+        </div>
+
+        <motion.div
+          {...buttonClick}
+          className="flex items-center justify-between px-28 py-2 gap-4 bg-lightOverlay backdrop:blur-sm cursor-pointer rounded-3xl hover:bg-gray-200 transition-all duration-150"
+        >
+          <FcGoogle className="text-3xl" />
+          <p className="font-semibold capitalize text-base text-headingColor">
+            Sign in with Google
+          </p>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Login;
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter as Router} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AnimatePresence>
+        <App />
+      </AnimatePresence>
     </Router>
   </React.StrictMode>
 );
