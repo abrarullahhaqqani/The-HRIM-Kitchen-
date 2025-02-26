cff495ca632f6965f8a89ecd92c2787ec28f3c9b
ishrat2004 <ishratalikhan004@gmail.com>
2025-02-27 01:16:10 +0530

    Thoda

--- The-HRIM-Kitchen--main/client/src/App.js
+++ The-HRIM-Kitchen--main/client/src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import {getAuth } from "firebase/auth";
+import { app } from "./config/firebase.config";
+import React, { use } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login, Main } from "./containers";
-
-const App = () => {
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { validateUserJWTToken } from "./api";
+import { setUserDetails } from "./context/actions/userActions";
+import { motion } from "framer-motion";
+import { FadeInOut } from "./animations";
+const App = () => { 
+  const firebaseAuth = getAuth(app);
+  const[isLoading,setIsLoading]=React.useState(false); 
+const dispatch=useDispatch();
+  useEffect(()=>{ 
+    setIsLoading(true); 
+   firebaseAuth.onAuthStateChanged((cred,user) => {
+           console.log("User is authenticated:", user);
+           if (cred) {
+             cred.getIdToken().then((token) => {
+               console.log(token);
+               validateUserJWTToken(token).then((data) => {
+              
+                dispatch(setUserDetails(data)); 
+               });
+             });
+           }
+         });
+  },[])
   return (
     //tailwind css styling classes added below
     <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
--- The-HRIM-Kitchen--main/client/src/index.js
+++ The-HRIM-Kitchen--main/client/src/index.js
@@ -6,17 +6,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 // import reportWebVitals from "./reportWebVitals";
-// import { createStore } from "redux";
-// import { Provider  } from "react-redux";
-
-// const myStore=createStore(myReducers);
+ import { createStore } from "redux";
+ import { Provider  } from "react-redux";
+import myReducers from "./context/reducers";
+ const myStore=createStore(myReducers,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <AnimatePresence>
-        <App />
+      <AnimatePresence> 
+        <Provider store={myStore}>
+          <App />
+        </Provider> 
       </AnimatePresence>
     </Router>
   </React.StrictMode>
