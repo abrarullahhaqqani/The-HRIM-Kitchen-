b08f7c5bcbd37b335051aabb24b70887c2d155ef
ishrat2004 <ishratalikhan004@gmail.com>
2025-03-01 18:28:53 +0530

    Done from our folder

--- client/src/context/actions/userActions.js
+++ client/src/context/actions/userActions.js
@@ -1,25 +1,13 @@
-// export const setUserDetails=(user)=>{
-//    return { 
-//     type: "SET_USER", 
-//     user:user,
-//    };
-
-// }; 
-
-// export const getUserDetails=()=>{
-//    return { 
-//     type: "GET_USER", 
-//    };
-// };
-//    return {
-//     type: "SET_USER",
-//     user:user,
-//    };
-
-// };
-
-// export const getUserDetails=()=>{
-//    return {
-//     type: "GET_USER",
-//    };
-// };
+ export const setUserDetails=(user)=>{
+    return { 
+        type: "SET_USER", 
+         user:user, // passing the user details
+          };
+      
+      }; 
+      
+      export const getUserDetails=()=>{
+         return { 
+          type: "GET_USER", 
+         };
+      };
