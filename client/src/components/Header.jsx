805621d04da763f15f692c8c78748be5d540f9a6
ishrat2004 <ishratalikhan004@gmail.com>
2025-03-08 11:15:22 +0530

    made some changes in the navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,9 @@ import { isActiveStyles, isNotActiveStyles } from '../utils/styles'
 import { motion } from 'framer-motion' 
 import { buttonClick } from '../animations'
 import {MdShoppingCart} from '../assets/icons'
+import {useSelector } from 'react-redux' 
 const Header = () => {
+  const user=useSelector(state=>state.user); 
   return <header className="fixed backdrop-blur-md z-50 inset-x-0 top-0 flex items-center justify-between px-6 md:px-20 py-6"> 
     <NavLink to="/" className="flex items-center justify-center gap-4"> 
         <img src={Logo} className="w-12 " alt="" /> 
@@ -24,6 +26,23 @@ const Header = () => {
             <p className="text-primary text-base font-semibold "> 2</p>
           </div>
        </motion.div>
+       {user ? <>
+       <div className="relative cursor-pointer"> 
+         <div className="w-12 h-12 rounded-full shadow-md cursor-pointer overflow-hidden
+         flex items-center justify-center "> 
+        <motion.img className="w-full h-full object-cover src={user?.picture ? user?.picture : Avatar } "
+        whileHover={{ scale:1.15 }}
+        referrerPolicy='no-referrer ' />  
+           
+          </div> 
+        </div> </> : <>
+        <NavLink to="/login"> 
+          <motion.button {...buttonClick} className="px-4 py-2 rounded-md shadow-md bg-lightOverlay border
+          border-red-300 cursor-pointer "> 
+           Login
+          </motion.button>
+        </NavLink>
+        </>}
     </nav>
   </header>
 }
